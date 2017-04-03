6816118cece3ef521ea2f12c9a4abdde9bda5277
JonathanMoroy <jonathan.moroy@gmail.com>
2017-04-03 12:11:01 +0200

    Correction des balises auto-fermante, ajout du non-scroll avec les flèches directionnelles et changement de police et des doubles quotes dans le css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // Fonction qui attend que le document soit ready
 $(function () {
+    // déclaration d'un tableau où se trouve les touches utiliser
+    var key = [37, 38, 39, 40];
     // Ajout d'un keypress à window où un switch détermine avec les keycode des flêches directionnelles la navigation entre les pages
     $(document).keydown(function (i) {
         // variable qui récupere les touches pressées
@@ -19,5 +21,11 @@ $(function () {
                 window.location = '#!/home';
                 break;
         }
+        // Annulation de l'event native des flèches directionnelles
+        if ($.inArray(press, key) > -1) {
+            i.preventDefault();
+            return false;
+        }
+        return true;
     }); // ~ Window Keypress
 }); // ~ Ready
